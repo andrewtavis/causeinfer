c271d71bcb2d87c3ad3242528b66394a002d8346
Andrew McAllister <andrewmcallister@Andrews-MacBook-Pro.local>
2019-11-25 15:32:08 +0100

    Split response_transformation based on binary and quaternary appraoches

---
+++ causeinfer/algorithms/binary_response_transformation.py
@@ -0,0 +1,121 @@
+# =============================================================================
+# The Binary Response Transformation Approach (Influential Marketing)
+# 
+# Based on
+# --------
+# - "Influential Marketing: A New Direct Marketing Strategy Addressing the Existence of Voluntary Buyers" (Lai, 2006)
+# 
+# Contents
+# --------
+# 1. BinaryResponseTransformation Class
+#   __init__
+#   __encode_binary_unknown_class
+#   __binary_regularization_weights
+#   fit
+#   predict
+# =============================================================================
+
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+from causeinfer.algorithms.base_models import TransformationModel
+
+class BinaryResponseTransformation(TransformationModel): # import as BRT
+
+    def __init__(self, model=LogisticRegression(n_jobs=-1), four_class=False, regularize=False):
+        """
+        Checks the attributes of the contorl and treatment models before assignment
+        """
+        try:
+            model.__getattribute__('fit')
+            model.__getattribute__('predict')
+        except AttributeError:
+            raise ValueError('Model should contains two methods: fit and predict.')
+        
+        self.model = model
+        self.regularize = regularize
+        
+    
+    def __encode_binary_unknown_class(self, y, w):
+        """
+        Derives which of the unknown Affected Positive or Affected Negative 
+        classes the unit could fall into
+        """
+        y_encoded = []
+        for i in range(y.shape[0]):
+            # Possible Affected Positives (TPs or CNs)
+            if self.is_treatment_positive(y[i], w[i]) or self.is_control_negative(y[i], w[i]):
+                y_encoded.append(1)
+
+            # Possible Affected Negatives (TNs or CPs)
+            elif self.is_treatment_negative(y[i], w[i]) or self.is_control_positive(y[i], w[i]):
+                y_encoded.append(0)
+        
+        return np.array(y_encoded)
+
+
+    def __binary_regularization_weights(self, y=None, w=None):
+        """
+        Regularization of binary classes is based on the positive and negative binary affectual classes
+        """
+        aff_pos_count, aff_neg_count = 0, 0
+        for i in range(y.shape[0]):
+            # The number of possible Affected Positives (TPs or CNs)
+            if self.is_treatment_positive(y[i], w[i]) or self.is_control_negative(y[i], w[i]):
+                aff_pos_count += 1
+
+            # The number of possible Affected Negatives (TNs or CPs)
+            elif self.is_treatment_negative(y[i], w[i]) or self.is_control_positive(y[i], w[i]):
+                aff_neg_count += 1
+
+        self.ratio_aff_pos = aff_pos_count / (aff_pos_count + aff_neg_count)
+        self.ratio_aff_neg = aff_neg_count / (aff_pos_count + aff_neg_count)
+        
+
+    def fit(self, X, y, w):
+        """
+        Parameters
+        ----------
+        X : numpy ndarray (num_units, num_features) : int, float 
+            Dataframe of covariates
+
+        y : numpy array (num_units,) : int, float
+            Vector of unit reponses
+
+        w : numpy array (num_units,) : int, float
+            Designates the original treatment allocation across units
+        
+        Returns
+        -------
+        - A trained model
+        """
+        y_encoded = self.__encode_binary_unknown_class(y, w)
+        if self.regularize:
+            self.__binary_regularization_weights(y, w)
+        
+        self.model.fit(X, y_encoded)
+        
+        return self
+
+
+    def predict(self, X_pred, regularize=False):
+        """
+        Parameters
+        ----------
+        X_pred : int, float
+             New data on which to make a prediction
+        
+        Returns
+        -------
+        - Predicted uplift for all units
+        """
+        pred_aff_pos = self.model.predict_proba(X_pred)[:, 1]
+        if self.regularize:
+            pred_aff_neg = self.model.predict_proba(X_pred)[:, 0]
+            
+            return (pred_aff_pos * self.ratio_aff_pos, pred_aff_neg * self.ratio_aff_neg)
+        else:
+            
+            return 2 * pred_aff_pos - 1
---
+++ causeinfer/algorithms/quaternary_response_transformation.py
@@ -0,0 +1,122 @@
+# =============================================================================
+# The Quaternary Response Transformation Approach (Class Variable Transformation)
+# 
+# Based on
+# --------
+# - "Mining for the truly responsive customers and prospects using true-lift modeling: 
+# Comparison of new and existing methods" (Kane, 2014)
+# 
+# Contents
+# --------
+# 1. QuaternaryResponseTransformation Class
+#   __init__
+#   __encode_quaternary_class
+#   __quaternary_regularization_weights
+#   fit
+#   predict
+# =============================================================================
+
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+from causeinfer.algorithms.base_models import TransformationModel
+
+class QuaternaryResponseTransformation(TransformationModel): # import as QRT
+
+    def __init__(self, model=LogisticRegression(n_jobs=-1), four_class=False, regularize=False):
+        """
+        Checks the attributes of the contorl and treatment models before assignment
+        """
+        try:
+            model.__getattribute__('fit')
+            model.__getattribute__('predict')
+        except AttributeError:
+            raise ValueError('Model should contains two methods: fit and predict.')
+        
+        self.model = model
+        self.regularize = regularize
+        
+
+    def __encode_quaternary_class(self, y, w):
+        """
+        Assigns quaternary (TP, CP, CN, TN) classes to units
+        """
+        y_encoded = []
+        for i in range(y.shape[0]):
+            if self.is_treatment_positive(y[i], w[i]):
+                y_encoded.append(0)
+            elif self.is_control_positive(y[i], w[i]):
+                y_encoded.append(1)
+            elif self.is_control_negative(y[i], w[i]):
+                y_encoded.append(2)
+            elif self.is_treatment_negative(y[i], w[i]):
+                y_encoded.append(3)
+        
+        return np.array(y_encoded)
+    
+    
+    def __quaternary_regularization_weights(self, y=None, w=None):
+        """
+        Regularization of quaternary classes is based on their treatment assignment
+        """
+        control_count, treatment_count = 0, 0
+        for el in w:
+            if el == 0.0:
+                control_count += 1
+            else:
+                treatment_count += 1
+
+        self.control_count = control_count
+        self.treatment_count = treatment_count
+        
+
+    def fit(self, X, y, w):
+        """
+        Parameters
+        ----------
+        X : numpy ndarray (num_units, num_features) : int, float 
+            Dataframe of covariates
+
+        y : numpy array (num_units,) : int, float
+            Vector of unit reponses
+
+        w : numpy array (num_units,) : int, float
+            Designates the original treatment allocation across units
+        
+        Returns
+        -------
+        - A trained model
+        """
+        y_encoded = self.__encode_quaternary_class(y, w)
+        if self.regularize:
+            self.__quaternary_regularization_weights(y, w)
+        
+        self.model.fit(X, y_encoded)
+        
+        return self
+
+
+    def predict(self, X_pred, regularize=False):
+        """
+        Parameters
+        ----------
+        X_pred : int, float
+             New data on which to make a prediction
+        
+        Returns
+        -------
+        - Predicted uplift for all units
+        """
+        pred_treatment_positive = self.model.predict_proba(X_pred)[:, 0]
+        pred_control_positive = self.model.predict_proba(X_pred)[:, 1]
+        pred_control_negative = self.model.predict_proba(X_pred)[:, 2]
+        pred_treatment_negative = self.model.predict_proba(X_pred)[:, 3]
+        if self.regularize:
+            
+            return (pred_treatment_positive / self.treatment_count + pred_control_negative / self.control_count) - \
+                (pred_treatment_negative / self.treatment_count + pred_control_positive / self.control_count)
+        else:
+            
+            return (pred_treatment_positive + pred_control_negative) - (pred_treatment_negative + pred_control_positive)
