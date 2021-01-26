f86b62bf951112a56fe759eefc46eb4551b3cfb4
Andrew McAllister <andrew.t.mcallister@gmail.com>
2021-01-26 19:30:03 +0100

    Fix formatting issue from black

--- causeinfer/__init__.py
+++
@@ -1,9 +0,0 @@
-from causeinfer.data import cmf_micro
-
-data_cmf_micro = cmf_micro.load_cmf_micro(
-    user_file_path="datasets/cmf_micro", format_covariates=True, normalize=True
-)
-
-df = pd.DataFrame(
-    data_cmf_micro["dataset_full"], columns=data_cmf_micro["dataset_full_names"]
-)
