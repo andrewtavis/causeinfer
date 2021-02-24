f5e4f54dbf2613b955da9c1e5ed89dbf85a78188
Andrew Tavis <andrew.t.mcallister@gmail.com>
2021-02-24 21:59:10 +0100

    Tests for data loaders

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -4,9 +4,54 @@ Evaluation Tests
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
+
 from causeinfer import evaluation
+
+
+def test_plot_cum_effect(monkeypatch):
+    monkeypatch.setattr(plt, "show", lambda: None)
+    assert True
+
+
+def test_plot_cum_gain(monkeypatch):
+    monkeypatch.setattr(plt, "show", lambda: None)
+    assert True
+
+
+def test_plot_qini(monkeypatch):
+    monkeypatch.setattr(plt, "show", lambda: None)
+    assert True
+
+
+def test_auuc_score():
+    assert True
+
+
+def test_qini_score():
+    assert True
+
+
+def test_get_batch_metrics():
+    assert True
+
+
+def test_plot_batch_metrics():
+    assert True
+
+
+def test_plot_batch_responses():
+    assert True
 
 
 def test_signal_to_noise(y_split, w_split):
     sn_ration = evaluation.signal_to_noise(y=y_split, w=w_split)
     assert type(sn_ration) == float or type(sn_ration) == np.float64
+
+
+def test_iterate_model():
+    assert True
+
+
+def test_eval_table():
+    assert True
