d92c7a74e63b7f598ddcfa2e46e634d87bbb89c4
Andrew Tavis <andrew.t.mcallister@gmail.com>
2021-04-28 18:04:26 +0200

    Version up for baseline requirements files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = "2019-2021, causeinfer developers (BSD License)"
 author = "causeinfer developers"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.2"
+release = "0.1.2.1"
 
 
 # -- General configuration ---------------------------------------------------
